309d002e27a07d440e8a2dc8d742427b76879b25
Hbib <habib.bekir@gmail.com>
2025-08-22 22:00:36 +0100

    Feat: added visitors stats

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ export default {
   components: { Navbar, Landing },
   data: () => ({
     darkMode: false,
+    statsMode: false,
   }),
   methods: {
     toggleDarkMode() {
@@ -12,6 +13,10 @@ export default {
     },
   },
   created() {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (urlParams.get("stats")) {
+      this.statsMode = true;
+    }
     if (
       window.matchMedia &&
       window.matchMedia("(prefers-color-scheme: dark)").matches
@@ -23,8 +28,15 @@ export default {
 </script>
 
 <template>
-  <div :class="{ dark: darkMode }">
+  <div v-if="!statsMode" :class="{ dark: darkMode }">
     <Navbar :darkMode="darkMode" :toggleDarkMode="toggleDarkMode" />
     <Landing />
+  </div>
+  <div v-else class="flex justify-center items-center min-h-dvh">
+    <a href="https://info.flagcounter.com/DSwk"
+      ><img
+        src="https://s01.flagcounter.com/count2/DSwk/bg_FFFFFF/txt_000000/border_CCCCCC/columns_5/maxflags_250/viewers_3/labels_0/pageviews_0/flags_0/percent_0/"
+        alt="Flag Counter"
+    /></a>
   </div>
 </template>
