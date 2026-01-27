a423f1c84413fdfa0bf2d4aeddc20e4e1f25b515
habib <habib.bekir@gmail.com>
2026-01-27 15:41:35 +0100

    feat: Contact page & UI improvements

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "flowbite/dist/flowbite";
 
 import { createApp } from "vue";
 import { createI18n } from "vue-i18n";
+import router from "./router";
 import App from "./App.vue";
 
 import fr from "./locales/fr.json";
@@ -24,5 +25,6 @@ const i18n = createI18n({
 const app = createApp(App);
 
 app.use(i18n);
+app.use(router);
 
 app.mount("#app");
